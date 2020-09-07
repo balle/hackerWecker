8ffcc20fa670d8203462624637a34aec14aafd38
Bastian Ballmann <balle@codekid.net>
2020-09-07 15:39:40 +0200

    retry getting url 3 times before giving up

--- web.go
+++ web.go
@@ -4,33 +4,45 @@ package hackerWecker
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
+	"os"
+	"time"
 )
 
-type fetchResult struct {
+type FetchResult struct {
 	Url     string
 	Content string
+	Error   error
 }
 
-func FetchUrl(url string, channel chan<- fetchResult) {
+func FetchUrl(url string, channel chan<- FetchResult) {
 	// Get url and return fetchResult struct
-	var result fetchResult
+	var result FetchResult
 	result.Url = url
+	rand.Seed(time.Now().Unix() * int64(os.Getpid()) / int64(os.Getppid()))
 
-	resp, err := http.Get(url)
-	defer resp.Body.Close()
-
-	if err != nil {
-		Speak(fmt.Sprintf("Error getting url %s: %v\n", url, err))
-	} else {
-		content, err := ioutil.ReadAll(resp.Body)
+	for tries := 0; tries < 3; tries++ {
+		fmt.Printf("Getting URL %s\n", url)
+		resp, err := http.Get(url)
+		defer resp.Body.Close()
 
 		if err != nil {
-			Speak(fmt.Sprintf("Error reading url %s: %v\n", url, err))
-			content = nil
+			Speak(fmt.Sprintf("Error getting url %s: %v\n", url, err))
+			result.Error = err
+		} else {
+			content, err := ioutil.ReadAll(resp.Body)
+
+			if err != nil {
+				Speak(fmt.Sprintf("Error reading url %s: %v\n", url, err))
+				result.Error = err
+			} else {
+				result.Content = string(content)
+				break
+			}
 		}
 
-		result.Content = string(content)
+		time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
 	}
 
 	channel <- result
