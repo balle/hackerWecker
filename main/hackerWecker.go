265ff774e65c95d40fadf9aef8246bd2997c7161
Bastian Ballmann <balle@codekid.net>
2020-09-28 11:17:27 +0200

    more flags and parallelism

--- main/hackerWecker.go
+++ main/hackerWecker.go
@@ -1,4 +1,4 @@
-// Wake up a hacker by reading news and play some music or podcast
+// Wake up a hacker by reading news and play some music and podcast
 package main
 
 import (
@@ -11,7 +11,10 @@ import (
 
 func main() {
 	var configFile = flag.String("config", "hackerWecker.json", "config file")
-	readFeeds := 0
+	var noMusic = flag.Bool("nomusic", false, "dont play music")
+	var noNews = flag.Bool("nonews", false, "dont read news")
+	var noPodcasts = flag.Bool("nopodcasts", false, "dont play podcasts")
+	var feeds []hackerWecker.Feed
 
 	flag.Parse()
 	err := hackerWecker.ReadConfig(*configFile)
@@ -20,30 +23,46 @@ func main() {
 		hackerWecker.LogFatal(fmt.Sprintf("Cannot read %s: %v", *configFile, err))
 	}
 
+	chanFeeds := make(chan hackerWecker.Feed, hackerWecker.NumFeeds())
+	chanPodcasts := make(chan hackerWecker.Feed, hackerWecker.NumPodcasts())
+
 	hackerWecker.Speak(hackerWecker.GetMsg("welcome"))
 
-	chanFeeds := make(chan hackerWecker.Feed)
-	go hackerWecker.FetchFeeds(hackerWecker.GetFeeds(), chanFeeds)
-
-	chanPodcasts := make(chan hackerWecker.Feed)
-	go hackerWecker.FetchFeeds(hackerWecker.GetPodcasts(), chanPodcasts)
-
-	hackerWecker.PlayMusic()
-	hackerWecker.Speak(hackerWecker.GetMsg("news"))
-
-	for i := 0; i < len(hackerWecker.GetFeeds()); i++ {
-		readFeeds += hackerWecker.ReadFeed(<-chanFeeds)
-		time.Sleep(1 * time.Second)
+	if !*noNews {
+		go hackerWecker.FetchFeeds(hackerWecker.GetFeeds(), chanFeeds)
 	}
 
-	if readFeeds == 0 {
-		hackerWecker.Speak(hackerWecker.GetMsg("nonews"))
+	if !*noPodcasts {
+		go hackerWecker.FetchFeeds(hackerWecker.GetPodcasts(), chanPodcasts)
 	}
 
-	hackerWecker.Speak(hackerWecker.GetMsg("podcasts"))
+	if !*noMusic {
+		go hackerWecker.PlayMusic()
+	}
 
-	for i := 0; i < len(hackerWecker.GetPodcasts()); i++ {
-		hackerWecker.PlayPodcast(<-chanPodcasts)
+	if !*noNews {
+		for i := 0; i < hackerWecker.NumFeeds(); i++ {
+			feeds = append(feeds, <-chanFeeds)
+		}
+
+		if len(feeds) == 0 {
+			hackerWecker.Speak(hackerWecker.GetMsg("nonews"))
+		} else {
+			hackerWecker.Speak(hackerWecker.GetMsg("news"))
+
+			for _, feed := range feeds {
+				hackerWecker.ReadFeed(feed)
+				time.Sleep(1 * time.Second)
+			}
+		}
+	}
+
+	if !*noPodcasts {
+		hackerWecker.Speak(hackerWecker.GetMsg("podcasts"))
+
+		for i := 0; i < len(hackerWecker.GetPodcasts()); i++ {
+			hackerWecker.PlayPodcast(<-chanPodcasts)
+		}
 	}
 
 	hackerWecker.Speak(hackerWecker.GetMsg("finished"))
